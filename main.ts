06892e2fde5016f05fce0e6b9e7f78a433163117
trivergod123 <82467786+trivergod123@users.noreply.github.com>
2021-04-13 13:37:48 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+scene.setBackgroundColor(1)
+let car = sprites.create(assets.image`car`, SpriteKind.Player)
+car.setPosition(80, 69)
+let nintendo_switch = sprites.create(assets.image`nintendo switch`, SpriteKind.Player)
+nintendo_switch.setPosition(21, 17)
+let track = sprites.create(assets.image`track`, SpriteKind.Player)
+track.setPosition(76, 17)
+let pokeball = sprites.create(assets.image`Pokeball`, SpriteKind.Player)
+pokeball.setPosition(128, 13)
+let Anime = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
